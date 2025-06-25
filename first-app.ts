a77f001cf0aa78bfa6b14718213314dc896bd664
woodpecker <hans.peter.specht@gmail.com>
2025-06-25 14:56:43 +0200

    Section 2.22

--- first-app.ts
+++ first-app.ts
@@ -4,18 +4,20 @@ let year: number = 2025;
 let isValid: boolean = true;
 const API_KEY = "abc";
 
-// or-operator
+// union types
 let userID: string | number = 123;
 userID = "Kennwort";
 
 // objects (never-types)
 // let user: object;
-let user: {
+type User = {
   name: string;
   age: number;
   isAdmin: boolean;
   id: string | number;
 };
+
+let user: User;
 
 user = {
   name: "Peter",
@@ -33,7 +35,89 @@ let hobbies: string[]; // number[], boolean[]
 hobbies = ["Sports", "Cooking", "Reading"];
 
 // functions
-function add(a: number, b: number): void {
+function add(a: number, b: number): number {
   const result = a + b;
-  console.log(result);
+  return result;
 }
+
+function substract(a: number, b: number): number {
+  const result = a - b;
+  return result;
+}
+
+// custom types, type aliases
+type CalcFn = (a: number, b: number) => number;
+
+function calculate(a: number, b: number, calcFn: CalcFn) {
+  calcFn(a, b);
+}
+
+calculate(2, 5, add);
+calculate(2, 5, substract);
+
+// interfaces
+interface Credentials {
+  password: string;
+  email: string;
+}
+
+// interface Credentials {
+//   mode: string;
+// }
+
+let creds: Credentials;
+
+creds = {
+  password: "abc",
+  email: "test@example.com"
+};
+
+class AuthCredentials implements Credentials {
+  password: string;
+  email: string;
+  userName: string;
+}
+
+function login(credentials: Credentials) {
+  // ...
+}
+
+login(new AuthCredentials());
+
+// merging types
+
+// type Admin = {
+//   permissions: string[];
+// };
+
+// type AppUser = {
+//   userName: string;
+// };
+
+// type AppAdmin = Admin & AppUser;
+// let admin: AppAdmin;
+
+// admin = {
+//   permissions: ["login"],
+//   userName: "Peter"
+// };
+
+interface Admin {
+  permissions: string[];
+}
+
+interface AppUser {
+  userName: string;
+}
+
+interface AppAdmin extends Admin, AppUser {}
+
+let admin: AppAdmin;
+
+admin = {
+  permissions: ["login"],
+  userName: "Peter"
+};
+
+// literal types
+let role: "admin" | "user" | "editor";
